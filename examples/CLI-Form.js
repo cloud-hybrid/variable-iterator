49a772342966d997b50698a2ff2e5332b473155b
Jacob B. Sanders <60639231+Segmentational@users.noreply.github.com>
2021-10-12 00:21:58 -0500

    [Development] - Example

--- examples/CLI-Form.js
+++ examples/CLI-Form.js
@@ -4,8 +4,20 @@ const name = await Prompt("Name" + ":" + " ", "Name");
 const height = await Prompt("Height" + ":" + " ", "Height");
 const age = await Prompt("Age" + ":" + " ", "Age");
 
-console.debug(name);
-console.debug(age);
-console.debug(height);
+console.debug("Name" + ":", name);
+console.debug("Height" + ":", height);
+console.debug("Age" + ":", age);
+
+const Data = {};
+
+[name, height, age].forEach(
+    (_) => {
+        const $ = Object.keys(_).pop();
+
+        Data[$] = Object.values(_).pop();
+    }
+);
+
+console.debug(Data);
 
 process.exit(0);
